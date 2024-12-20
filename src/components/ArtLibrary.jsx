7a73bde42f4f91acfc236bde93ecb7bb929ac7bf
s-Aura-v <slamich2@oswego.edu>
2024-12-20 01:27:57 -0500

    Added Art Library

--- src/components/ArtLibrary.jsx
+++ src/components/ArtLibrary.jsx
@@ -1,42 +1,43 @@
 import F24_P1 from '../assets/ArtLibrary/F24_P1.png'
 import F24_P2 from '../assets/ArtLibrary/F24_P2.png'
+import F24_P2_1 from '../assets/ArtLibrary/F24_P2_1.png'
+import F24_P3 from '../assets/ArtLibrary/F24_P3.png'
+import F24_P3_1 from '../assets/ArtLibrary/F24_P3.1.png'
+import S24_P2_1 from '../assets/ArtLibrary/S24_P2_1.png'
+import S24_P2_2 from '../assets/ArtLibrary/S24_P2_2.png'
+import S24_P2_3 from '../assets/ArtLibrary/S24_P2_3.png'
+import S24_P3_1 from '../assets/ArtLibrary/S24_P3_1.png'
+import S24_P3_2 from '../assets/ArtLibrary/S24_P3_2.png'
+
+import '../styles/artlibrary.css'
 export function ArtLibrary() {
     return (
         <>
-            <div className="row">
-                <div className="column">
-                    <img src={F24_P1}/>
-                    <img src={F24_P2}/>
-                    <img src="falls2.jpg"/>
-                    <img src="paris.jpg"/>
-                    <img src="nature.jpg"/>
-                    <img src="mist.jpg"/>
-                    <img src="paris.jpg"/>
-                </div>
-                <div className="column">
-                    <img src="underwater.jpg"/>
-                    <img src="ocean.jpg"/>
-                    <img src="wedding.jpg"/>
-                    <img src="mountainskies.jpg"/>
-                    <img src="rocks.jpg"/>
-                    <img src="underwater.jpg"/>
-                </div>
-                <div className="column">
-                    <img src="wedding.jpg"/>
-                    <img src="rocks.jpg"/>
-                    <img src="falls2.jpg"/>
-                    <img src="paris.jpg"/>
-                    <img src="nature.jpg"/>
-                    <img src="mist.jpg"/>
-                    <img src="paris.jpg"/>
-                </div>
-                <div className="column">
-                    <img src="underwater.jpg"/>
-                    <img src="ocean.jpg"/>
-                    <img src="wedding.jpg"/>
-                    <img src="mountainskies.jpg"/>
-                    <img src="rocks.jpg"/>
-                    <img src="underwater.jpg"/>
+
+            <div className="album">
+                <div className="responsive-container-block bg">
+                    <div className="responsive-container-block img-cont">
+                        <img className="img"
+                             src={F24_P1}/>
+                        <img className="{img}"
+                             src={S24_P2_1}/>
+                    </div>
+                    <div className="responsive-container-block img-cont">
+                        <img className="img img-big"
+                             src={F24_P2}/>
+                        <img className="img img-big "
+                             src={F24_P2_1}/>
+                        <img className="img img-big "
+                             src={S24_P2_2}/>
+                    </div>
+                    <div className="responsive-container-block img-cont">
+                        <img className="img"
+                             src={F24_P3}/>
+                        <img className="img"
+                             src={F24_P3_1}/>
+                        <img className="img"
+                             src={S24_P2_3}/>
+                    </div>
                 </div>
             </div>
         </>
