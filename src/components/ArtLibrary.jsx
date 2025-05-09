3c39febf09113823daf1ee8adaa0014f03b3bee0
s-Aura-v <slamich2@oswego.edu>
2025-05-09 19:09:22 -0400

    New project structure updated

--- src/components/ArtLibrary.jsx
+++ src/components/ArtLibrary.jsx
@@ -8,34 +8,12 @@ import S24_P2_2 from '../assets/ArtLibrary/S24_P2_2.png'
 import S24_P2_3 from '../assets/ArtLibrary/S24_P2_3.png'
 import S24_P3_1 from '../assets/ArtLibrary/S24_P3_1.png'
 import S24_P3_2 from '../assets/ArtLibrary/S24_P3_2.png'
+import Fun_Text_Art from '../assets/ArtLibrary/FUN_Text_Art.png'
 
 import '../styles/artlibrary.css'
 export function ArtLibrary() {
     return (
         <>
-
-            {/*<div className="album">*/}
-            {/*    <div className="flex">*/}
-            {/*        <img className="flex-3by4"*/}
-            {/*             src={F24_P1}/>*/}
-
-            {/*        <img className="flex-3by4"*/}
-            {/*             src={S24_P2_1}/>*/}
-            {/*        <img className="flex-3by4 "*/}
-            {/*             src={S24_P2_2}/>*/}
-            {/*        <img className="flex-3by4"*/}
-            {/*             src={S24_P2_3}/>*/}
-            {/*        <img className="flex-2by1"*/}
-            {/*             src={F24_P2}/>*/}
-            {/*        <img className="flex-4by3 "*/}
-            {/*             src={F24_P2_1}/>*/}
-            {/*        <img*/}
-            {/*             src={F24_P3}/>*/}
-            {/*        <img className="img"*/}
-            {/*             src={F24_P3_1}/>*/}
-
-            {/*    </div>*/}
-            {/*</div>*/}
 
             <div id="images-wrapper">
                 <div className="flex">
@@ -51,12 +29,13 @@ export function ArtLibrary() {
                          src={F24_P2}/>
                     <img className="flex-4by3 "
                          src={F24_P2_1}/>
+
                     <img
                         src={F24_P3}/>
                     <img className="img"
                          src={F24_P3_1}/>
-
                 </div>
+                <div></div>
             </div>
         </>
     )
--- src/pages/test.jsx
+++ src/pages/test.jsx
@@ -13,16 +13,16 @@ export function TestingGround() {
                                 <p className="card-description">{project.description}</p>
 
                                 <div className="project-footer">
+                                    <div className="project-tags">
+                                        {project.tags.map((tag, i) => (
+                                            <div key={i} className="project-tag">{tag}</div>
+                                        ))}
+                                    </div>
                                     <div className="project-links">
                                         {project.links.map((link, i) => (
                                             <a key={i} href={link.url} target="_blank" rel="noopener noreferrer">
                                                 <img src={link.icon} alt={link.name}/>
                                             </a>
-                                        ))}
-                                    </div>
-                                    <div className="project-tags">
-                                        {project.tags.map((tag, i) => (
-                                            <div key={i} className="project-tag">{tag}</div>
                                         ))}
                                     </div>
                                 </div>
