73a59b6acbe957448eec4184cf3578ba14ce9a33
s-Aura-v <slamich2@oswego.edu>
2026-05-18 17:53:29 -0400

    [App] Title Page
    1. Added CSS and HTML for the TV style background page

--- src/App.svelte
+++ src/App.svelte
@@ -8,11 +8,36 @@
     import './app.css'
 </script>
 
-<section id="page">
-    <img src={TV} class="mask1"/>
-    <div class="info">
-        <h2 class="title">s-aura-v</h2>
+<!--<section id="page">-->
+<!--    <img src={TV} class="mask1"/>-->
+<!--    <div class="info">-->
+<!--        <h2 class="title">s-aura-v</h2>-->
+<!--    </div>-->
+
+
+
+<!--    &lt;!&ndash;    </div>&ndash;&gt;-->
+<!--</section>-->
+
+<section id="page" class="parent">
+    <!-- TV Background Layer -->
+    <div class="bg-image">
+        <enhanced:img src={TV} alt="tv-screen"/>
     </div>
 
-    <!--    </div>-->
+    <!-- Any content above the background -->
+    <div class="content-layer">
+        <!-- Empty spacer pushing the title down slightly if needed, or keeping it centered -->
+        <div class="spacer"></div>
+
+        <h1 class="title">s-aura-v</h1>
+
+        <div class="nav-bar">
+            <ul>
+                <li>Projects</li>
+                <li>Gallery</li>
+                <li>Misc</li>
+            </ul>
+        </div>
+    </div>
 </section>
